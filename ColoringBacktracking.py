2f70eeea4bbdf0ddc0b1768e7250d1a9e4eb4adb
Mariane Souza <mariane.elisa@ufrpe.br>
2024-09-20 17:33:27 -0300

    Modifications to Greedy and Backtracking algorithms for graph coloring

--- ColoringBacktracking.py
+++ ColoringBacktracking.py
@@ -1,27 +1,27 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def ifSafe(v, c, G, color):
-    for i in G.neighbors(v):
-        if color[i] == c:
-            return False
-    return True
-
 def coloring_backtracking(G, num=None) -> dict:
+    # Inicializa o dicionário de cores com indice -1
     K = {v:-1 for v in G.nodes()}
+
     if num is not None:
         colors = [x for x in range(num)]
     else:
         colors = [x for x in range(100)]
 
+    # Inicio da recursão
     if coloring_backtracking_util(G, K, colors, 0):
         return K
     else:
         return None
     
 def coloring_backtracking_util(G, K, colors, v):
+    # Se todos os vértices foram coloridos, então a solução foi encontrada
     if v == len(K):
         return True
+    
+    # Para cada cor disponível, verifica se é segura
     for c in colors:
         if ifSafe(v, c, G, K):
             K[v] = c
@@ -30,18 +30,52 @@ def coloring_backtracking_util(G, K, colors, v):
             K[v] = -1
     return False
 
+def ifSafe(v, c, G, color):
+    # Verifica se a cor c é segura para o vértice v
+    for i in G.neighbors(v):
+        if color[i] == c:
+            return False
+    return True
 
-def plot_graph(G, K):
+
+def plot_graph(G, K, color=None):
     pos = nx.spring_layout(G)
     if K is not None:
-        nx.draw(G, pos, with_labels=True, node_color=[K[v] for v in G.nodes()], cmap=plt.cm.rainbow, node_size=500)
+        if color is not None:
+            nx.draw(G, node_color = K, with_labels=True, node_size=500)
+        else:
+            nx.draw(G, pos, with_labels=True, node_color=[K[v] for v in G.nodes()], cmap=plt.cm.rainbow, node_size=500)
         plt.show()
     else:
         print("Nenhuma solução de coloração encontrada.")
 
 if __name__ == "__main__":
     G = nx.Graph()
-    G.add_edges_from([(0,1), (0,2), (0,3), (2,3), (2,4), (3,4), (4,5)])
+    G.add_edges_from([(0,1), (1,3), (0,3), (1,2), (2,3), (1,5), (4,5), (4,7), (7,6), (5,6), (3,7)])
+    M = {}
+    for v in G.nodes():
+        M[v] = "#b8b8b8"
+    plot_graph(G, M)
+
+    input()
+
     K = coloring_backtracking(G,3)
     plot_graph(G, K)
-    
+
+    input()
+
+    G.add_edges_from([(0,1), (0,2), (1,3), (0,3), (1,2), (2,3), (1,5), (4,5), (4,6), (4,7), (7,6), (5,6), (3,7)])
+    M = {}
+    for v in G.nodes():
+        M[v] = "#b8b8b8"
+    plot_graph(G, M)
+
+    input()
+
+    K = coloring_backtracking(G,3)
+    plot_graph(G, K)
+
+    input()
+
+    K = coloring_backtracking(G)
+    plot_graph(G, K)
--- ColoringGriedy.py
+++ ColoringGriedy.py
@@ -2,50 +2,75 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 def coloring_griedy(G, num=None) -> dict:
-    # inicializa o dicionário de cores com indice -1 
+    # Inicializa o dicionário de cores com indice -1 
     # para mostrar que não foi colorido
     K = {v:-1 for v in G.nodes()} 
     
-    # se num for None, então o número de cores é igual ao grau
-    # do vértice de maior grau + 1
+     
     if num is not None:
-        colors = [x for x in range(num-1)]
+        colors = [x for x in range(num)]
     else:
         colors = [x for x in range(100)]
 
     for node in K:
-        # inicializa a lista de cores disponíveis
+        # Inicializa a lista de cores disponíveis
         available = [True]*len(colors)
+        # Para cada vizinho do nó, se ele já foi colorido, então
+        # a cor dele não está disponível
         for neighbor in G.neighbors(node):
             if K[neighbor] != -1:
                 available[K[neighbor]] = False
+
+        # Atribui a menor cor disponível ao nó
         for color in range(len(colors)):
             if available[color]:
                 K[node] = color
                 break
+
+        # Se não houver nenhuma cor disponível, então não há solução
+        if all([n == False in available for n in available]):
+            if K[node] == -1:
+                return None
     return K
 
 
 def plot_graph(G, K):
+    if K is None:
+        print("Nenhuma solução de coloração encontrada.")
+        return
     pos = nx.spring_layout(G)
     nx.draw(G, pos, with_labels=True, node_color=[K[v] for v in G.nodes()], cmap=plt.cm.rainbow, node_size=500)
     plt.show()
 
 if __name__ == "__main__":
     G = nx.Graph()
+    G.add_edges_from([(0,1), (1,3), (0,3), (1,2), (2,3), (1,5), (4,5), (4,7), (7,6), (5,6), (3,7)])
+    M = {}
+    for v in G.nodes():
+        M[v] = "#b8b8b8"
+    plot_graph(G, M)
 
-    # v = str(input("Digite os vértices separados por vírgula: "))
-    # v = v.split(',')
-    # for i in v:
-    #     G.add_node(i)
-    #     a = str(input("A quais vértices o vértice " + i + " está conectado? "))
-    #     a = a.split(',')
-    #     for j in a:
-    #         G.add_edge(i, j)
-    # n = int(input("Digite o número de cores: "))
-    # if n == 0:
-    #     K = coloring_griedy(G)
+    input()
 
-    G.add_edges_from([(0,1), (0,2), (0,3), (2,3), (6,7), (5,7), (7,4), (2,4), (3,4), (4,5), (4,6), (5,6)])
     K = coloring_griedy(G,3)
     plot_graph(G, K)
+
+    input()
+
+    G.add_edges_from([(0,1), (0,2), (1,3), (0,3), (1,2), (2,3), (1,5), (4,5), (4,6), (4,7), (7,6), (5,6), (3,7)])
+    M = {}
+    for v in G.nodes():
+        M[v] = "#b8b8b8"
+    plot_graph(G, M)
+
+    input()
+    
+    K = coloring_griedy(G,3)
+    plot_graph(G, K)
+
+    input()
+
+    K = coloring_griedy(G)
+    plot_graph(G, K)
+
+
